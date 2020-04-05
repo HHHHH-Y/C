092083f6b6200fe0c795160276d4b4eee1f1e5b9
HHHHH-Y <984495875@qq.com>
2020-04-05 21:07:22 +0800

    代码优化及调试

---
+++ day_4_5/test.c
@@ -0,0 +1,82 @@
+#define _CRT_SECURE_NO_WARNINGS
+// Debug是可以调试的版本, Release是不可以调试的版本
+// 程序员要是要调试代码, 就需要在Debug版本中进行调试0
+#include <stdio.h>
+
+//int test(int x, int y) {
+//	int z = x + y;
+//	return z;
+//}
+//int main() {
+//	int arr[20] = { 0 };
+//	int i = 0;
+//	int sum = test(2, 3);
+//	for (i = 0; i < 10; i++) {
+//		arr[i] = i;
+//	}
+//
+//	for (i = 10; i < 20; i++) {
+//		arr[i] = i; // 断点
+//	}
+//	return 0;
+//}
+
+//void test2() {
+//	printf("hehe\n");
+//}
+//void test1() {
+//	test2();
+//}
+//void test() {
+//	test1();
+//}
+//
+//int main() {
+//	test();
+//	return 0;
+//}
+
+// 1! + 2! + 3! + ... + n!
+//int main() {
+//	int n = 0;
+//	scanf("%d", &n); // 3
+//	int i = 0;
+//	int ret = 1;
+//	int j = 0;
+//	int sum = 0;
+//	for (j = 1; j <= n; j++) {
+//		ret = 1;
+//		for (i = 1; i <= j; i++) {
+//			ret *= i;   // n!
+//		}
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//int main() {
+//	int n = 0;
+//	scanf("%d", &n); // 3
+//	int i = 0;
+//	int ret = 1;
+//	int j = 0;
+//	int sum = 0;
+//	for (j = 1; j <= n; j++) {
+//		ret *= j;  // n!
+//		sum += ret;
+//	}
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//int main() {
+//	int i = 0;
+//	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	for (i = 0; i <= 12; i++) {
+//		arr[i] = 0;
+//		printf("hehe\n");
+//	}
+//	return 0;
+//}
+
